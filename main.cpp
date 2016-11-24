dc4c91789104560bf9a00087ed76963ba40f12b1
Aleksandr Kosykh <kik9399@mail.ru>
2016-11-24 23:01:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,51 @@
 #include <iostream>
 using namespace std;
-struct Guitar // Структура гитара
+struct Guitar 
 {
-    char model[64]; // Модель
-    int year; // Год выпуска
-    char country[64]; // Страна
+	char model[64]; 
+	int year; 
+	char country[64]; 
 };
-struct Musician
+struct Musician 
 {
-    char name[128];
-    Guitar gtr;
+	char name[128];
+	Guitar gtr;
 
 };
 int main()
 {
-    Musician *pG;
-    int n;
-    cout << "n=";
-    cin >> n;
-    pG = (Musician *)malloc(n*sizeof(Musician));
-    for (int i = 0; i<n; i++)
-    {
-        cin.clear();
-        cout << "guitar N: " << i + 1;
-        cout << "\nname: ";
-        cin >> pG[i].name;
-        cin.clear();
+	Musician *pG;
+	int n;
+	cout << "Number of guitars = ";
+	cin >> n;
+	pG = (Musician *)malloc(n * sizeof(Musician));
+	for (int i = 0; i<n; i++)
+	{
+		cin.clear();
+		cout << "guitar N: " << i + 1;
+		cout << "\nMusician's name: ";
+		cin >> pG[i].name;
+		cin.clear();
 
-        cout << "\nmodel: ";
-        cin >> pG[i].gtr.model;
-        cin.clear();
+		cout << "\nmodel: ";
+		cin >> pG[i].gtr.model;
+		cin.clear();
 
-        cout << "\nYear: ";
-        cin >> pG[i].gtr.year;
-        cin.clear();
+		cout << "\nYear: ";
+		cin >> pG[i].gtr.year;
+		cin.clear();
 
-        cout << "\ncountry: ";
-        cin >> pG[i].gtr.country;
-        cin.clear();
-    }
-    for (int i = 0; i<n; i++)
-        cout << pG[i].name << " " << pG[i].gtr.model << " " << pG[i].gtr.year <<" " << pG[i].gtr.country  << endl;
-    free (pG);
-    return 0;
+		cout << "\ncountry: ";
+		cin >> pG[i].gtr.country;
+		cin.clear();
+	}
+	for (int i = 0; i<n; i++)
+		cout << pG[i].name << " " << pG[i].gtr.model << " " << pG[i].gtr.year << " " << pG[i].gtr.country << endl;
+	int k = 0;
+	for (int i = 0; i < n; i++) 
+		if (pG[i].gtr.year < 1980) k++;
+	if (k) cout << "Musicians with rare guitars: " << k << endl;
+	free(pG);
+	system("pause");
+	return 0;
 }
